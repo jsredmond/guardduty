65ae5c590cf6175a87e26c47e11e432c41609df6
“Jeremy <norrinscott@gmail.com>
2022-08-23 15:23:51 -0400

    test

---
+++ guardduty.tf
@@ -0,0 +1,22 @@
+# Enable GuardDuty
+resource "aws_guardduty_detector" "MyDetector" {
+  enable = true
+
+  datasources {
+    s3_logs {
+      enable = true
+    }
+    kubernetes {
+      audit_logs {
+        enable = true
+      }
+    }
+    malware_protection {
+      scan_ec2_instance_with_findings {
+        ebs_volumes {
+          enable = true
+        }
+      }
+    }
+  }
+}
