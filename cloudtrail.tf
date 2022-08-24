0503b6d582bc106ca6e5e17a799eef72d75616c0
“Jeremy <norrinscott@gmail.com>
2022-08-23 20:51:02 -0400

    Created cloudtrail.tf file to enable CloudTrail

---
+++ cloudtrail.tf
@@ -0,0 +1,56 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "main" {
+  name                          = "tf-trail-main"
+  s3_bucket_name                = aws_s3_bucket.main.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = true
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket        = "cloudtrail-18932740192347"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "${aws_s3_bucket.main.arn}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "${aws_s3_bucket.main.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
